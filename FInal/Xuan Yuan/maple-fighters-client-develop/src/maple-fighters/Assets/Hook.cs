8c635bffb791abe7d386d0a1e5742adfdb7bd6e2
voidii <ruoqian@umail.ucsb.edu>
2020-12-06 00:15:21 -0800

    final project shibi bug

--- FInal/Xuan Yuan/maple-fighters-client-develop/src/maple-fighters/Assets/Hook.cs
+++ FInal/Xuan Yuan/maple-fighters-client-develop/src/maple-fighters/Assets/Hook.cs
@@ -13,30 +13,31 @@ public class Hook : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        //m_SpriteRenderer.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(in_animation){
-            animator.ResetTrigger("Hook");
-        }
-
-        if(animator.GetCurrentAnimatorStateInfo(0).IsName("hook")){
-            m_SpriteRenderer.enabled = true;
-            in_animation = true;
-        }
-        else{
-            m_SpriteRenderer.enabled = false;
-            in_animation = false;
-        }
-        
-        if(Input.GetMouseButtonDown(1))
+        if(Input.GetMouseButtonDown(1) && !in_animation)
         {
-            
-            in_animation = true;
-            animator.SetTrigger("Hook");
+            //m_SpriteRenderer.enabled = true;
+            //Vector2 translation = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+            //Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            //float angle =360-Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            //transform.eulerAngles = new Vector3(0, 0, angle);
+            StartCoroutine(PlayAndDisappear());
+            //animator.SetTrigger("Hook");
         }
 
+
+    }
+
+    IEnumerator PlayAndDisappear(){
+        in_animation = true;
+        animator.SetTrigger("Hook");
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        //m_SpriteRenderer.enabled = false;
+        in_animation = false;
     }
 }
